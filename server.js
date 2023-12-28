7d775b4d28ad0a6803a129eefc86ffacbe92d98f
Dara16 <just4oludee@yahoo.com>
2023-12-28 17:28:29 -0500

    dependencies added to server

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const dotenv = require('dotenv');
 
+const app = express();
+
+app.use(bodyParser.json());
+
+dotenv.config({ path: './config/config.env' });
