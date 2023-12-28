787b3de9afbf70a917ef4c9da0372fb556b9989a
Dara16 <just4oludee@yahoo.com>
2023-12-28 18:06:06 -0500

    mongo db connected successfully

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db')
+
+dotenv.config({ path: './config/config.env' });
+
+connectDB()
 
 const app = express();
 
 app.use(bodyParser.json());
 
-dotenv.config({ path: './config/config.env' });
-
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5001
 
 const server = app.listen(PORT, () => {
     console.log(`Server is listening on PORT: ${PORT}`)
