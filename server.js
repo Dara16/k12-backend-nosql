243783de6f4d0a641b9cd51c8798563b2f817e1d
Dara16 <just4oludee@yahoo.com>
2023-12-28 17:38:59 -0500

    server configured

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const app = express();
 app.use(bodyParser.json());
 
 dotenv.config({ path: './config/config.env' });
+
+const PORT = process.env.PORT || 5000
+
+const server = app.listen(PORT, () => {
+    console.log(`Server is listening on PORT: ${PORT}`)
+})
